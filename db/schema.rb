329acff062bcb4694f81e3599c7637466dc85fd8
Lukáš Toman <toman-lukas@seznam.cz>
2024-10-08 21:51:44 +0200

    added 'config/database.yml' to .gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_10_08_143604) do
-  create_table "ticket_types", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "ticket_types", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.decimal "price", precision: 10
     t.string "name"
     t.integer "quantity"
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_08_143604) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tickets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "tickets", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "ticket_type_id", null: false
     t.string "digest", default: "", null: false
     t.datetime "scanned_at"
@@ -31,7 +31,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_08_143604) do
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name"
     t.string "secret"
     t.datetime "created_at", null: false
